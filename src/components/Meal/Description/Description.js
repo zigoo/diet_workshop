eed34d23dc4076733ef95796663869f64059245d
zigoo <xzigoo@gmail.com>
2017-07-28 12:15:26 +0200

    title change

--- src/components/Meal/Description/Description.js
+++ src/components/Meal/Description/Description.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid, Row, Col } from 'react-bootstrap';
+//import { Grid, Row, Col } from 'react-bootstrap';
 import './style.css';
 
 class MealDescription extends Component {
