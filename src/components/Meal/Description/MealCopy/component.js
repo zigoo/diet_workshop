d8abba7669ed8e21f47396fb3f6ee0cec2f9b3fa
zigoo <xzigoo@gmail.com>
2017-08-06 22:56:47 +0200

    add tooltips

--- src/components/Meal/Description/MealCopy/component.js
+++ src/components/Meal/Description/MealCopy/component.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import { Glyphicon, Button} from 'react-bootstrap';
+import { Glyphicon, Button, Tooltip, OverlayTrigger} from 'react-bootstrap';
 import './styles.css';
 
 class MealCopy extends Component {
   render() {
     const {desc} = this.props.selectedId;
+    const tooltip = (
+      <Tooltip placement="bottom" className="in" id="three">
+         skopiuj do schowka!
+      </Tooltip>);
+
     return (
       <div className="MealCopy">
-        <CopyToClipboard text={desc}>
-          <span id="copy">
-            <Button 
-              bsStyle="default" className="def" 
-              bsSize="large" >  
-                kopiuj 
-                  <Glyphicon glyph="copy"></Glyphicon>
-            </Button>
-          </span>
-        </CopyToClipboard>
+        <OverlayTrigger placement="bottom" overlay={tooltip} >
+          <CopyToClipboard text={desc}>
+            <span id="copy">
+              <Button 
+                bsStyle="default" className="def" 
+                bsSize="large" >  
+                   kopiuj 
+                <Glyphicon glyph="copy"></Glyphicon>
+              </Button>
+            </span>
+          </CopyToClipboard>
+        </OverlayTrigger>
       </div>
     );
   }
 }
 
 export default MealCopy;
- 
--- src/components/Utils/Search/component.js
+++ src/components/Utils/Search/component.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import List from './List/component.js';
 import meals from '../../../helpers/meals.js';
 import Collapsible from '../Collapsible/Collapsible.js';
-import { FormGroup, FormControl } from 'react-bootstrap';
-import { Row } from 'react-bootstrap';
+import { Row, FormGroup, FormControl, Tooltip} from 'react-bootstrap';
 import './style.css';
   
 
 class Search extends Component {
   constructor(){
     super();
-    this.state = {value: '', meals:meals, items:[]};
+    this.state = {value: '', meals:meals, items:[], visible: true};
     this.handleChange = this.handleChange.bind(this);
+    this.handleVisibility = this.handleVisibility.bind(this);
   }
   handleChange(e) {
     let initialList = this.state.meals;
@@ -22,26 +22,42 @@ class Search extends Component {
     this.setState({items: filteredList}); 
   }
 
+  handleVisibility() {
+    this.setState({visible: !this.state.visible});
+  }
+
   render() {
-     
+    const visibleVar = this.state.visible ? '' : 'not';
+    const tooltip = ( 
+      <div className={"visible "+ visibleVar} >
+        <Tooltip placement="left" className="in" id="one">
+             rozwiń i znajdź przepis
+        </Tooltip>
+      </div>
+    );
+
     return (
       <div className="Search">
-        <Collapsible title='Szukaj'>       
-          <div className="Utils_find-box">
-            <Row>           
-              <form>
-                <FormGroup controlId="Utils_find">
-                  <FormControl bsSize="small" bsClass="form-control form-control_custom"
-                    type="text" value={this.state.value}
-                    placeholder="" onChange={this.handleChange} />
-                </FormGroup>
-              </form>  
-            </Row>
-            <Row> 
-              <List items={this.state.items} />
-            </Row>
-          </div>
-        </Collapsible>
+        {tooltip}    
+        <div id="temp" onClick={this.handleVisibility}>
+          <Collapsible title='Szukaj'>       
+            <div className="Utils_find-box">
+              <Row>           
+                <form>
+                  <FormGroup controlId="Utils_find">
+                    <FormControl 
+                      bsSize="small" bsClass="form-control form-control_custom"
+                      type="text" value={this.state.value}
+                      placeholder="" onChange={this.handleChange} autoFocus="true" />
+                  </FormGroup>
+                </form>  
+              </Row>
+              <Row> 
+                <List items={this.state.items} />
+              </Row>
+            </div>
+          </Collapsible>
+        </div>
       </div>
     );
   }
