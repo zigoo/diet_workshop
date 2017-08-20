72af1669e98f4e0811d3cc7434a12bd57bc8fe95
zigoo <xzigoo@gmail.com>
2017-08-20 12:27:34 +0200

    styles

--- src/components/Utils/Hoc/Searchid.js
+++ src/components/Utils/Hoc/Searchid.js
@@ -16,10 +16,7 @@ function SearchForId(ComponentHoc) {
   function mapStateToProps(state) {
     let selDate = state.meal.selectedDate;
     let day =  parseInt(selDate.substring(8),10);
- 
-   /* if (day > 10) 
-      day = 1 + (Math.floor(Math.random()*10));*/
-      
+
     return {
       mealId: state.meal.meals.find(meal => ((meal.type === state.meal.selectedMeal) && (meal.day === day)) ),
       selectedDate: state.meal.selectedDate
